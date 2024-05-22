fef1d29c33d4322da5b3bd8eaa298ce1120fa47e
melissa-cavalcante <melissa.massimino@gmail.com>
2024-05-22 09:59:10 -0300

    implementação da classe horista

--- src/Horista.java
+++ src/Horista.java
@@ -1,3 +1,19 @@
 public class Horista extends Empregado {
-    
+    private double horas;
+
+    public Horista(String nome, String endereco, double h){
+        super(nome, endereco);
+        horas = h;
+    }
+
+    public double getHoras() {
+        return horas;
+    }
+
+    public void setHoras(double horas) {
+        this.horas = horas;
+    }
+    public void calcularSalario() {
+        salario = horas * 50;
+    }
 }
